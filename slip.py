1ca8c271b8cea6703f5fddd17f5e3e72708c9342
beyondonor <beyondonor@gmail.com>
2023-04-24 23:57:51 +0530

    slip

--- slip.py
+++ slip.py
@@ -36,6 +36,24 @@ def slip(num,cus):
     img.save("output/images/"+nums+".jpg")
 
 
+def slip1(num,cus):
+    img = Image.open('input/pic.jpeg')
+    d1 = ImageDraw.Draw(img)
+    myFont = ImageFont.truetype('input/font/KGBlankSpaceSketch.ttf', 40)
+    myFont1 = ImageFont.truetype('Arial', 45)
+    nums = str(num)
+    
+    # to eliminate the empty names
+    k = str(cus)
+    if k == "nan":
+        cus = "  "
+    else:
+        print()
+    
+    d1.text((400, 300),str(cus),(0,0,0),anchor="mm",font=myFont)
+    d1.text((678, 50),"#"+nums,(0,0,0),font=myFont1,align='centre')
+   # img.show()
+    img.save("output/images/"+nums+".jpg")
 # %%
 
 
@@ -61,12 +79,12 @@ nums = df.index.tolist()
 
 # %%
 for i,j in zip(names,nums):
-    #print(i,j)
-    #print(type(j))
-    #print("\n")
+    if len(names[1])>24:
+        slip(j,i)
+    else:
+        slip1(j,i)
 
-    slip(j,i)
-
+print("Done")
 # %%
 
 
